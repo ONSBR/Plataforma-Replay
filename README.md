c791641efc120768731f18266d3f3b1ce8f9cd56
Philippe Moneda <philippemoneda@gmail.com>
2018-11-29 11:37:16 -0200

    ajustes de log e TODO

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Plataforma-Replay
 Este é um serviço para realizar a gravação de execução dos eventos para fins de debug e replicação de cenários de execução
+
+# TODO
+
+* Fazer o processo de upload da fita de gravação (Mexer na UI para adicinar a forma de fazer upload)
+* Adicionar função para iniciar o "play" da fita
+* Restaurar backup do banco de dados
+* Reemitir os eventos na plataforma
--- tape/util.go
+++ tape/util.go
@@ -8,10 +8,13 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/labstack/gommon/log"
 )
 
 func compress(src string, writers ...io.Writer) error {
 
+	log.Info("Start compressing tape")
 	if _, err := os.Stat(src); err != nil {
 		return fmt.Errorf("Unable to tar files - %v", err.Error())
 	}
@@ -23,38 +26,31 @@ func compress(src string, writers ...io.Writer) error {
 
 	tw := tar.NewWriter(gzw)
 	defer tw.Close()
-
+	log.Info("starting add files from src = ", src)
 	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
-
 		if err != nil {
 			return err
 		}
-
 		header, err := tar.FileInfoHeader(fi, fi.Name())
 		if err != nil {
 			return err
 		}
-
 		header.Name = strings.TrimPrefix(strings.Replace(file, src, "", -1), string(filepath.Separator))
-
+		log.Info("add file = ", header.Name)
 		if err := tw.WriteHeader(header); err != nil {
 			return err
 		}
-
 		if !fi.Mode().IsRegular() {
 			return nil
 		}
-
 		f, err := os.Open(file)
 		defer f.Close()
 		if err != nil {
 			return err
 		}
-
 		if _, err := io.Copy(tw, f); err != nil {
 			return err
 		}
-
 		return nil
 	})
 }
