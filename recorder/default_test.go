2fcf7dade1a5b3604abcb4948683120578274f4c
Philippe Moneda <philippemoneda@gmail.com>
2018-12-11 17:24:33 -0200

    adicionar testes

--- recorder/default_test.go
+++ recorder/default_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/ONSBR/Plataforma-Replay/tape"
+
 	"github.com/ONSBR/Plataforma-EventManager/domain"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -18,5 +20,46 @@ func TestShouldRec(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(rec.IsRecording(), ShouldBeTrue)
 		os.RemoveAll("./tapes")
+	})
+}
+
+func TestShouldEjectTape(t *testing.T) {
+	systemID := "ec498841-59e5-47fd-8075-136d79155705"
+	tapeID := "ec498841-59e5-47fd-8075-136d79155705_1544212971.rec"
+	Convey("should rec an event for a system", t, func() {
+		os.Setenv("TAPES_PATH", "../test_files")
+		tape.Restore(systemID, tapeID)
+		rec := newDefaultRecorder(systemID)
+		_, err := rec.Eject()
+		So(err, ShouldBeNil)
+		tapes, _ := rec.AvailableTapesToDownload(systemID)
+		So(len(tapes), ShouldBeGreaterThan, 1)
+		for _, s := range tapes {
+			if s != tapeID {
+				So(tape.Delete(s), ShouldBeNil)
+				break
+			}
+		}
+	})
+}
+
+func TestShouldGetEventFromSegment(t *testing.T) {
+	systemID := "ec498841-59e5-47fd-8075-136d79155705"
+	tapeID := "ec498841-59e5-47fd-8075-136d79155705_1544212971.rec"
+	Convey("should get an event from segment", t, func() {
+		os.Setenv("TAPES_PATH", "../test_files")
+		tape.Restore(systemID, tapeID)
+		rec := newDefaultRecorder(systemID)
+		currTape, _ := rec.GetOrCreateTape(systemID)
+		for _, seg := range currTape.Segments {
+			if seg.IsEvent() {
+				evt, err := seg.Event(currTape)
+				So(err, ShouldBeNil)
+				So(evt, ShouldNotBeNil)
+				break
+			}
+
+		}
+		os.RemoveAll("../test_files/" + systemID)
 	})
 }
